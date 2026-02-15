3298665b7c4311c18d87cb2d9df3d12837e8b737
MinhTien-ishi <minhtienpham429@gmail.com>
2026-02-15 09:37:21 +0900

    Building Header Logic #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,15 +2,16 @@ import classNames from "classnames/bind";
 import styles from "./AccountItem.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Image from "~/components/Image";
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx("wrapper")}>
-            <img
+            <Image
                 className={cx("avatar")}
-                src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/996219793a42f662d63d498deeba394e~tplv-tiktokx-cropcenter:300:300.webp?dr=14577&refresh_token=11fc8d80&x-expires=1770969600&x-signature=p9rMQAE1tcdveQZFfCA72Z2RdW8%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=c1333099&idc=my2"
+                src="https://16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/996219793a42f662d63d498deeba394e~tplv-tiktokx-cropcenter:300:300.webp?dr=14577&refresh_token=11fc8d80&x-expires=1770969600&x-signature=p9rMQAE1tcdveQZFfCA72Z2RdW8%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=c1333099&idc=my2"
                 alt="Hoaa"
             />
             <div className={cx("info")}>
